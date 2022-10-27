7ec08b9d03f7e98d24d92561948a9825820412ce
hamwah <115349352+hamwah@users.noreply.github.com>
2022-10-28 01:41:17 +0200

    Add files via upload

---
+++ party.js/homework.js
@@ -0,0 +1,16 @@
+let age=prompt("plz enter your age");
+
+if(age<18){
+    alert ("you are too young")
+
+}
+else if (age>35)
+{
+    alert ("you are too old")
+}
+
+else {  
+let name =prompt("plz enter your name");
+console.log("welcome " + name + " to the party");
+
+}
